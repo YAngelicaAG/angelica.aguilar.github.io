a5d1699c233453cb5334d0684bcb8bb311f00b4e
Angelica Aguilar <yeimi.aguilar@aguayo.co>
2024-02-27 23:16:56 -0500

    (fix) - Se ajusta la identación y comentarios del webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,38 +3,37 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'docs')
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'docs')
-        },
-        port: 8080,
-        hot: true,
-        liveReload: true // Habilita la recarga automática del navegador
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'css/styles_aa_site.css' // Ruta de salida para el archivo CSS
-        }),
-        new HtmlWebpackPlugin({
-            template: './docs/index.html' // Ruta al archivo HTML de origen
-        })
-    ]
+	mode: 'development', // Modo de desarrollo
+	entry: './src/index.js', // Archivo de entrada de JavaScript
+	output: {
+		filename: 'bundle.js', // Nombre del archivo JavaScript de salida
+		path: path.resolve(__dirname, 'docs') // Directorio de salida
+	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'docs') // Directorio base para servir archivos estáticos
+		},
+		port: 8080, // Puerto del servidor de desarrollo
+		hot: true, // Habilita la recarga en caliente (Hot Module Replacement - HMR)
+		liveReload: true // Habilita la recarga automática del navegador
+	},
+	module: {
+		rules: [{
+			test: /\.scss$/, // Patrón para identificar archivos Sass
+			use: [
+				MiniCssExtractPlugin.loader, // Extracción de CSS en un archivo separado
+				'css-loader', // Interpretación de archivos CSS
+				'sass-loader' // Compilación de archivos Sass a CSS
+			]
+		}]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'css/styles_aa_site.css' // Ruta de salida para el archivo CSS
+		})
+		,
+		new HtmlWebpackPlugin({
+			template: './docs/index.html' // Ruta al archivo HTML de origen
+		})
+	]
 };
